fae60a5ca8d1c8a935d0f4ddac29827a1548c5ca
Mooka <47282413+mooka007@users.noreply.github.com>
2024-05-25 23:57:07 +0100

    utils + agent

---
+++ front/src/components/Agent/Agent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Hero from "../Hero/Hero";
+import SearchBar from "../utils/SearchBar";
+import ExpertSection from "../Utils/ExpertSection";
+
+function Agent() {
+  return (
+    <div className="Agent">
+      <Header />
+      <Hero header="We have Expert Agents ready to support your goals" />
+
+      <ExpertSection
+        header="Meet Expert Agents"
+        text="We have certified experts ready to attend to you"
+      />
+      <SearchBar />
+      
+      {/* <FeaturedProperty
+        header="Featured Properties"
+        text="Top Rated Properties"
+      /> */}
+      <div className="mt-20"></div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Agent;
