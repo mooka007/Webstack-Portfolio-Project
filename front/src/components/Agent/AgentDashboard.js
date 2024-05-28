984c79dc683a59699b2f556c15c288ad7f698678
Mooka <47282413+mooka007@users.noreply.github.com>
2024-05-28 18:58:26 +0100

    agent dashboard

---
+++ front/src/components/Agent/AgentDashboard.js
@@ -0,0 +1,31 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import AgentHeroBanner from "./AgentHeroBanner";
+
+import ExpertSection from "../Utils/ExpertSection";
+
+function AgentDashboard() {
+
+  return (
+    <div className="Agent">
+      <Header />
+
+  
+        <AgentHeroBanner
+          header="Agent One"
+          text="Expert dealer in Residential Homes and Luxury apartments. Licensed in Canada and Asia. Trusted by experts and companies with 4.5 rating in Trustpilot."
+          // TODO implement: switch over to Contact Agent whenever page is viewed by public user.
+          label="List a Property"
+        />
+      
+
+      <ExpertSection
+        header="Meet Expert Agents"
+        text="We have certified experts ready to attend to you"
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default AgentDashboard;
